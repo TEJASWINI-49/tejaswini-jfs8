4e843b918edfb9bfa52b237d3e7c25e2802e4a35
TEJASWINI-49 <eteja4949@gmail.com>
2024-07-05 20:43:53 +0530

    Add files via upload

---
+++ DAY-19/operators.js
@@ -0,0 +1,88 @@
+//Arithematic operators
+/*let a = 10;
+let b = 20;
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(b / a);
+console.log(a ** b);
+
+//Assignment operators
+var a1 = 5;
+a1 = a1 + 1;
+console.log(a1);
+a1 += 2;
+console.log(a1);
+var a1 = 5;
+a1 = a1 - 1;
+console.log(a1);
+a1 -= 1;
+console.log(a1);
+var a1 = 6;
+a1 = a1 * 2;
+console.log(a1);
+a1 *= 2;
+console.log(a1);
+var a1 = 7;
+a1 = a1 / 7;
+console.log(a1);
+a1 /= 7;
+console.log(a1);
+var a1 = 8;
+a1 = a1 % 2;
+console.log(a1);
+a1 %= 2;
+console.log(a1);
+*/
+//equals
+var a = 10;
+var b = "10";
+console.log(a != b);
+//strictly not equals
+console.log(a !== b);
+var a = 10;
+var b = 20;
+console.log(a > b);
+console.log(a >= b);
+console.log(a < b);
+console.log(a <= b);
+//Logical Operators
+console.log("Logical Operators");
+var age = 20;
+var voter = true;
+console.log(age >= 18);
+console.log(voter == true);
+console.log("Logical Operators");
+console.log(age >= 18 && voter == true);
+console.log(age >= 18 || voter == true);
+console.log(age !== 18);
+
+//Unary Operator
+console.log("Unary Operators");
+var x = 5;
+console.log(+x);
+var y = 6;
+console.log(-y);
+//Teranry operators
+console.log("Ternary Operators");
+var age = 80;
+var result = age > 18 ? "Can Vote" : "cannot vote";
+console.log(result);
+//Increment and Decrement
+console.log(" Increment and Decrement");
+var a1 = 100;
+console.log(a1++);
+var a2 = 200;
+console.log(++a2);
+var b1 = 90;
+
+console.log(b1--);
+var b2 = 100;
+console.log(--b1);
+
+//String Concactenation
+var a1 = "hello";
+var b1 = "Codegnan";
+console.log(a1 + " " + b1);
