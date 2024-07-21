a7fd282ebe92fbe9664246f14f1630dc9684e118
TEJASWINI-49 <eteja4949@gmail.com>
2024-07-21 22:24:32 +0530

    Add files via upload

---
+++ DAY-25/sam.js
@@ -0,0 +1,17 @@
+console.log("strings");
+//creation of Strings
+var x = "Tejaswini";
+console.log(x);
+var a = a.length();
+//Access elements
+console.log(x[4]);
+var v1 = "teja";
+var v2 = "swini";
+var v3 = v1 + "" + v2 + "!";
+console.log(v3);
+console.log(v1);
+
+x1 = "teja";
+x2 = "swini";
+var x3 = x1.concat("", x2, "!");
+console.log(x3);
